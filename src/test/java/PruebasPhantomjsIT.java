3b80a7b2b0c47a8cbeeddb7b6ae10c5e75a12c7d
Christian Dopico <christian.dopico@example.com>
2022-05-14 02:41:11 +0200

    Agregada prueba funcional tabla de votos

--- src/test/java/PruebasPhantomjsIT.java
+++ src/test/java/PruebasPhantomjsIT.java
@@ -77,4 +77,55 @@ class PruebasPhantomjsIT {
 
     }
 
+    @Test
+    void setVoteOtherPlayerTest() {
+        System.out.println("Tercera Prueba");
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setJavascriptEnabled(true);
+        caps.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY, "/usr/bin/phantomjs");
+        caps.setCapability(PhantomJSDriverService.PHANTOMJS_CLI_ARGS,
+                new String[] { "--web-security=no", "--ignore-ssl-errors=yes" });
+        driver = new PhantomJSDriver(caps);
+        driver.navigate().to("http://localhost:8080/Baloncesto/");
+
+        // Se marca el radio button para votar por otro jugador
+        driver.findElement(By.name("R4")).click();
+        // Se introduce en su campo de texto el nombre de un jugador
+        String namePlayer = "Brandon Davies";
+        driver.findElement(By.name("txtOtros")).sendKeys(namePlayer);
+        // Se realiza el envío del voto realizado
+        driver.findElement(By.name("B1")).click();
+        // Se vuelve a la página principal
+        driver.findElement(By.tagName("a")).click();
+
+        // Se abre la página para visualizar los votos
+        driver.findElement(By.name("B3")).click();
+        driver.findElement(By.tagName("a")).click();
+
+        boolean expectedResult = true;
+        // Se obtiene la lista de los valores de las votaciones de cada uno de los
+        // jugadores
+        WebElement tableVotes = driver.findElement(By.tagName("table"));
+        ArrayList<WebElement> rows = new ArrayList<>(tableVotes.findElements(By.tagName("tr")));
+        // Se elimina la cabecera de las columnas de la tabla
+        rows.remove(0);
+
+        int counter = 0;
+        boolean verify = false;
+        // Comprueba entre los distintos jugadores con votos al jugador al que se le ha
+        // dado un voto y se comprueba que ha recibido dicho voto
+        while (!verify && counter < rows.size()) {
+            List<WebElement> cells = rows.get(counter).findElements(By.tagName("td"));
+            // Comprueba que sea el jugador introducido para la votación y mira que tiene un
+            // voto
+            if (cells.get(1).getText().trim().equals(namePlayer) && cells.get(2).getText().trim().equals("1")) {
+                verify = true;
+            }
+            counter++;
+        }
+
+        assertEquals(expectedResult, verify,
+                "El jugador no ha recibido el voto.");
+    }
+
 }
