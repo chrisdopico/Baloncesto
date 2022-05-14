13d91f85782f34e2c55119442ff20650d65ff639
Christian Dopico <christian.dopico@example.com>
2022-05-14 02:05:48 +0200

    Agregada prueba funcional votos a cero

--- src/test/java/PruebasPhantomjsIT.java
+++ src/test/java/PruebasPhantomjsIT.java
@@ -28,4 +28,52 @@ private static PhantomJSDriver driver=null;
         driver.quit();
     }
 
+    @Test
+    void setVotosToCeroTest()
+    {
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setJavascriptEnabled(true);
+        caps.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY,"/usr/bin/phantomjs");
+        caps.setCapability(PhantomJSDriverService.PHANTOMJS_CLI_ARGS, new
+                String[] {"--web-security=no", "--ignore-ssl-errors=yes"});
+        driver = new PhantomJSDriver(caps);
+        driver.navigate().to("http://localhost:8080/Baloncesto/");
+
+        //Se realiza la limpieza de votos
+        driver.findElement(By.name("B2")).click();
+        //Se abre la página para visualizar los votos
+        driver.findElement(By.name("B3")).click();
+
+        //Se obtiene la lista de los valores de las votaciones de cada uno de los jugadores
+        WebElement tableVotes = driver.findElement(By.tagName("table"));
+        ArrayList<WebElement> rows = new ArrayList<>(tableVotes.findElements(By.tagName("tr")));
+        //Se elimina la cabecera de las columnas de la tabla
+        rows.remove(0);
+        ArrayList<WebElement> listWebElementsVotes = new ArrayList<>();
+        //Se obtiene la lista de votos de cada jugador de cada fila
+        for(int i = 0; i < rows.size(); i++)
+        {
+            List<WebElement> cells = rows.get(i).findElements(By.tagName("td"));
+            //Se coge solo el campo de los votos de la fila
+            listWebElementsVotes.add(cells.get(2));
+        }
+
+        boolean expectedResult = true;
+        boolean verify = true;
+        int counter = 0;
+        //Se comprueba que el valor de los votos de cada jugador están a cero
+        while(verify && counter < listWebElementsVotes.size())
+        {
+            if(!listWebElementsVotes.get(counter).getText().trim().equals("0"))
+            {
+                verify = false;
+            }
+            counter++;
+        }
+
+        assertEquals(expectedResult, verify,
+                "No se ha realizado la puesta a 0 de los votos.");
+    }
+
+
 }
